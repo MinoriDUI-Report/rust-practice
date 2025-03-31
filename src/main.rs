a0ca27ab77b878559923a5e04ed82379b9b968d0
JiWon Kim <andrewkimswe@gmail.com>
2025-03-31 15:12:12 +0900

    data race 학습

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,22 @@
-extern crate rand;
-
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() {
-    println!("Guess the number!");
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
-    let secret_number = rand::rng().random_range(1..=100);
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        // trim 후 숫자로 변환, 실패 시 continue
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
     }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
